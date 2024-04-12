f93cf03abf4efbc738026f4ce3a2adf38e3f67c2
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2024-04-13 00:24:28 +0200

    update footer

--- src/components/layouts/PageFooter.tsx
+++ src/components/layouts/PageFooter.tsx
@@ -1,26 +1,15 @@
 import {
-  DOCS_URL,
-  DISCORD_URL,
-  TELEGRAM_URL,
   TWITTER_URL,
   GITHUB_URL,
-  REDDIT_URL,
   EMAIL_URL,
-  MEDIUM_URL,
-  GITBOOK_URL,
+
 } from "utils/urls";
 
 
 import GitHubIcon from "@icons/GitHubIcon";
 import TwitterIcon from "@icons/TwitterIcon";
-import RedditIcon from "@icons/RedditIcon";
-import DiscordIcon from "@icons/DiscordIcon";
-import TelegramIcon from "@icons/TelegramIcon";
-import MediumIcon from "@icons/MediumIcon";
-import GitBookIcon from "@icons/GitBookIcon";
 import EmailIcon from "@icons/EmailIcon";
-import DocsIcon from "@icons/DocsIcon";
-import FIRN_LOGO from "assets/firn.svg";
+import MUNUS_LOGO from "assets/munus.png";
 
 
 export function PageFooter() {
@@ -36,13 +25,13 @@ export function PageFooter() {
         </div>
         <p className="text-base text-zinc-400 text-center font-telegrama">
           <span className="opacity-50">
-            FREEDOM
+            Made with love and Amstel in our blood
           </span>
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-600 via-cyan-600 to-blue-600 active:from-cyan-700 active:to-blue-700">
-            <img src={FIRN_LOGO} className="ml-4 mr-4 h-5 w-auto inline -mt-1.5"/>
+            <img src={MUNUS_LOGO} className="ml-4 mr-4 h-5 w-auto inline -mt-1.5"/>
           </span>
           <span className="opacity-50">
-             FIRN
+             MUNUS
           </span>
         </p>
       </div>
@@ -60,40 +49,13 @@ function FooterSocialSection() {
         className="text-blue-400 hover:text-blue-400"
       />
       <FooterSocialLink
-        href={DISCORD_URL}
-        IconComponent={DiscordIcon}
-        className="text-indigo-500 hover:text-indigo-500"
-      />
-      {/*<FooterSocialLink*/}
-      {/*  href={TELEGRAM_URL}*/}
-      {/*  IconComponent={TelegramIcon}*/}
-      {/*  className="group-hover:text-sky-400 dark:text-sky-400"*/}
-      {/*/>*/}
-      <FooterSocialLink
         href={EMAIL_URL}
         IconComponent={EmailIcon}
-      />
-      {/*<FooterSocialLink*/}
-      {/*  href={MEDIUM_URL}*/}
-      {/*  IconComponent={MediumIcon}*/}
-      {/*/>*/}
-      <FooterSocialLink
-        href={DOCS_URL}
-        IconComponent={DocsIcon}
-        className="text-blue-700 hover:text-blue-700"
-      />
-      <FooterSocialLink
-        href={GITBOOK_URL}
-        IconComponent={GitBookIcon}
       />
       <FooterSocialLink
         href={GITHUB_URL}
         IconComponent={GitHubIcon}
         className="text-zinc-100 hover:text-zinc-100"
-      />
-      <FooterSocialLink
-        href={REDDIT_URL}
-        IconComponent={RedditIcon}
       />
     </>
   );
