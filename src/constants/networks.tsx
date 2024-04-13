5956d8217dfa47103025fceeacf559beea338d82
Augusto F. Hack <hack.augusto@gmail.com>
2024-04-13 09:38:05 +0200

    rename Optimism -> Base

--- src/constants/networks.tsx
+++ src/constants/networks.tsx
@@ -3,7 +3,7 @@ import BASE_ICON from "assets/icons/base.png";
 
 export const CHAIN_PARAMS = {
   "Base": {
-    name: "Optimism",
+    name: "Base",
     blockExplorerUrl: "https://optimistic.etherscan.io",
     image: BASE_ICON,
     etherscanApiUrl: "https://api-optimistic.etherscan.io",
